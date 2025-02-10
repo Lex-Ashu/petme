a2236b2cb9df6b44d2f662f614ef175143c317f5
Lex-Ashu <thisisashuchoudhary@gmail.com>
2025-02-10 18:15:33 +0530

    settings

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.db import models
 
 class Subscription(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     plan = models.CharField(max_length=100)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
