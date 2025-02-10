6562a7e1b7a41d2520bca26a2872470bde229a3f
Lex-Ashu <thisisashuchoudhary@gmail.com>
2025-02-10 17:09:24 +0530

    basic structures

--- pets/views.py
+++ pets/views.py
@@ -1,25 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Pet
-from django.contrib.auth.decorators import login_required
 
-@login_required
 def pet_list(request):
-    pets = Pet.objects.filter(owner=request.user)
+    pets = Pet.objects.all()
     return render(request, 'pets/pet_list.html', {'pets': pets})
 
-@login_required
 def pet_detail(request, pet_id):
     pet = Pet.objects.get(id=pet_id)
     return render(request, 'pets/pet_detail.html', {'pet': pet})
-
-@login_required
-def add_pet(request):
-    if request.method == "POST":
-        name = request.POST['name']
-        breed = request.POST['breed']
-        age = request.POST['age']
-        medical_history = request.POST['medical_history']
-        pet = Pet(owner=request.user, name=name, breed=breed, age=age, medical_history=medical_history)
-        pet.save()
-        return redirect('pet_list')
-    return render(request, 'pets/add_pet.html')
