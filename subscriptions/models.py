cfccd22565400720861860d74ffed981c85195b9
Lex-Ashu <thisisashuchoudhary@gmail.com>
2025-02-10 17:16:20 +0530

    review_model

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from users.models import User
 
-class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField()
-    comment = models.TextField()
-    review_date = models.DateTimeField(auto_now_add=True)
+class Subscription(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    plan = models.CharField(max_length=100)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.user.username} - {self.rating}"
+        return f"{self.user.username} - {self.plan}"
