dbfcd0809d3139be5c93a46a57544334ac1fadba
Lex-Ashu <thisisashuchoudhary@gmail.com>
2025-02-10 18:13:02 +0530

    users model

--- users/models.py
+++ users/models.py
@@ -1,9 +1,20 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    is_premium = models.BooleanField(default=False)
+    # Add your custom fields here if any
 
-    def __str__(self):
-        return self.username
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # Change related_name to avoid conflict
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',  # Change related_name to avoid conflict
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
